59dc232d7dafe2d01937fe2c4e0600596374f7dd
Sachira-Induwara <sachirainduwara72@gmail.com>
2026-07-24 22:00:31 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,31 +1,27 @@
-const { File } = require('megajs');
-const fs = require('fs');
-
-/**
- * Zip or upload session folder/file to Mega
- * @param {string} filePath - Path to the session file or directory
- * @returns {Promise<string>} - Mega file link / Session ID
- */
-async function uploadToMega(filePath) {
-    return new Promise((resolve, reject) => {
-        try {
-            const stream = fs.createReadStream(filePath);
-            const storage = new File({
-                // Mega credentials context (Anonymous upload)
-            });
-
-            // Standard anonymous upload logic to Mega
-            File.upload(filePath, async (err, file) => {
-                if (err) return reject(err);
-                const link = await file.link();
-                // Link එකේ 'SESSION_ID:' prefix එක යොදා Session ID එකක් ලෙස Return කිරීම
-                const sessionId = 'SESSION_ID:' + link.replace('https://mega.nz/file/', '');
-                resolve(sessionId);
-            });
-        } catch (error) {
-            reject(error);
-        }
-    });
+const mega = require("megajs");
+const auth = {
+    email: 'sachirainduwara72@gmail.com',
+    password: 'Sachi@2010',
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
-module.exports = { uploadToMega };
+const upload = (data, name) => {
+    return new Promise((resolve, reject) => {
+        try {
+            const storage = new mega.Storage(auth, () => {
+                data.pipe(storage.upload({name: name, allowUploadBuffering: true}));
+                storage.on("add", (file) => {
+                    file.link((err, url) => {
+                        if (err) throw err;
+                        storage.close()
+                        resolve(url);
+                    });
+                });
+            });
+        } catch (err) {
+            reject(err);
+        }
+    });
+};
+
+module.exports = { upload };
